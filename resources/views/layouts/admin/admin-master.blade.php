47f3c912c6ec448a88a64450e8f67ba263d60480
Noman73 <noman.eng73@gmail.com>
2022-11-14 09:48:59 +0600

    14-11-2022

--- resources/views/layouts/admin/admin-master.blade.php
+++ resources/views/layouts/admin/admin-master.blade.php
@@ -7,10 +7,10 @@
 	<meta name="author" content="" />
 	<meta name="robots" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Jobick : Job Admin Bootstrap 5 Template" />
-	<meta property="og:title" content="Jobick : Job Admin Bootstrap 5 Template" />
-	<meta property="og:description" content="Jobick : Job Admin Bootstrap 5 Template" />
-	<meta property="og:image" content="https://jobick.dexignlab.com/xhtml/social-image.png" />
+	<meta name="description" content="Bekalpo : Online buy sale platform" />
+	<meta property="og:title" content="Bekalpo : Online buy sale platform" />
+	<meta property="og:description" content="Bekalpo : Online buy sale platform" />
+	<meta property="og:image" content="{{asset('storage/logo/'.$company->logo)}}" />
 	<meta name="format-detection" content="telephone=no">
 	
 	<!-- PAGE TITLE HERE -->
